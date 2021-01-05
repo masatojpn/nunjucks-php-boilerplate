68db9b73d047a8ba8ea3f23906b500670731db47
Masato Nakamura <masatojpn@icloud.com>
2021-01-05 14:38:58 +0900

    Gulpタスク用コンフィグファイルを作成

---
+++ conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  paths: {
+    src: {
+      root: 'src/',
+      template: 'src/template/',
+      html: 'src/template/page/',
+      json: './src/template/data/site.json',
+      assets: 'src/assets/'
+    },
+    dest: {
+      root: 'html/'
+    }
+  }
+}
